6f1f2655dfdc24b0c36d51a4219d63ae8702b586
Gustav Tiger <gustav@tiger.name>
2012-06-16 22:50:40 +0200

    Updated model and removed old view
    - Participant is now Entity
    - AssociatedFile is now File
    - Combined sender receiver into entity
    - Added created and updated fields to all models
    - Added notes to most models

--- models.py
+++ models.py
@@ -1,33 +1,54 @@
 from django.db import models
 
 class Category(models.Model):
-	name = models.CharField(max_length=100)
-	parent = models.ForeignKey('self')
-	
-	def __unicode__(self):
-		return self.name
+    """ Category """
+    name             = models.CharField(max_length=100)
+    parent           = models.ForeignKey('self', null=True)
+    note             = models.TextField(blank=True)
 
-class Participant(models.Model):
-	name = models.CharField(max_length=200)
-	
-	def __unicode__(self):
-		return self.name
+    created          = models.DateTimeField(auto_now_add=True)
+    updated          = models.DateTimeField(auto_now=True)
 
-class AssociatedFile(models.Model):
-	name = models.CharField(max_length=400)
-	file = models.FileField(upload_to='files')
-	
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
+
+class Entity(models.Model):
+    """ Entity (might represent both physical persons and legal persons) """
+    name             = models.CharField(max_length=200)
+    note             = models.TextField(blank=True)
+
+    created          = models.DateTimeField(auto_now_add=True)
+    updated          = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
+
+class File(models.Model):
+    """ File """
+    name             = models.CharField(max_length=400)
+    file             = models.FileField(upload_to='files')
+    note             = models.TextField(blank=True)
+
+    created          = models.DateTimeField(auto_now_add=True)
+    updated          = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
 
 class Transaction(models.Model):
-	category = models.ForeignKey(Category)
-	sender = models.ForeignKey(Participant, related_name='transaction_sender')
-	receiver = models.ForeignKey(Participant, related_name='transaction_receiver')
-	order_date = models.DateField()
-	transaction_date = models.DateField()
-	amount = models.IntegerField()
-	transactions = models.ManyToManyField('self')
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	associated_files = models.ManyToManyField(AssociatedFile)
+    """ Transaction """
+    category         = models.ForeignKey(Category, related_name='transactions')
+    entity           = models.ForeignKey(Entity, related_name='transactions')
+    order_date       = models.DateField()
+    transaction_date = models.DateField()
+    amount           = models.IntegerField()
+    transactions     = models.ManyToManyField('self')
+    files            = models.ManyToManyField(File)
+    note             = models.TextField(blank=True)
+
+    created          = models.DateTimeField(auto_now_add=True)
+    updated          = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u'%d %s' % (self.amount, self.note)
+
