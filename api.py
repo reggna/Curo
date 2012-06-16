28ea82336f487cc97495a9713ab23596e2da4ee1
Gustav Tiger <gustav@tiger.name>
2012-06-16 22:58:20 +0200

    Added JSON based API using Tastypie
    - Should be quite complete
    - Missing file upload and download
      - Downloads should be easy to fix
      - Uploads harder :(

---
+++ api.py
@@ -0,0 +1,35 @@
+from tastypie import fields
+from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from Curo.models import Transaction, Category, Entity, File
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'category'
+        authorization = Authorization()
+
+class EntityResource(ModelResource):
+    class Meta:
+        queryset = Entity.objects.all()
+        resource_name = 'entity'
+        authorization = Authorization()
+
+class FileResource(ModelResource):
+    class Meta:
+        queryset = File.objects.all()
+        resource_name = 'file'
+        authorization = Authorization()
+
+class TransactionResource(ModelResource):
+
+    category = fields.ForeignKey(CategoryResource, 'category')
+    sender   = fields.ForeignKey(EntityResource, 'sender')
+    receiver = fields.ForeignKey(EntityResource, 'receiver')
+
+    class Meta:
+        queryset = Transaction.objects.all()
+        resource_name = 'transaction'
+        authorization = Authorization()
+        excludes = ['associated_files']
+
