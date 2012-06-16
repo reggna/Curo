cc98a6c31820f7b486aaf436b407f45de1133d26
Gustav Tiger <gustav@tiger.name>
2012-06-16 22:53:23 +0200

    Added admin interface support

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Curo.models import Transaction, Category, Entity, File
+
+admin.site.register(Transaction)
+admin.site.register(Category)
+admin.site.register(Entity)
+admin.site.register(File)
+
