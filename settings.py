e0ec302701dcd931f158737cc02033be45269f5c
Gustav Tiger <gustav@tiger.name>
2012-06-23 20:36:44 +0200

    Added settings (supports local settings) and manage script
    Django will read settings.py and then the local file local_settings.py.
    This means you can add local settings such as database settings and
    secret keys here.

---
+++ settings.py
@@ -0,0 +1,27 @@
+# Django settings for Curo project.
+
+# Enable debug
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# URL Config
+ROOT_URLCONF = 'urls'
+
+# Installed applicatiosn
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'tastypie',
+    'api',
+)
+
+# Load local settings from local_settings.py
+try:
+    from local_settings import *
+except ImportError, exp:
+    pass
+
