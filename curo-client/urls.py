0851bf905dfbe897daca69001d1d5458bf6098b6
Erik Stenbäcka <stenbacka@gmail.com>
2012-07-01 18:37:29 +0200

    Adds empty curo-client app.

--- curo-client/urls.py
+++ curo-client/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^', include('curo-api.urls')),
-    url(r'^', include('curo-client.urls'))
+
 )
 
