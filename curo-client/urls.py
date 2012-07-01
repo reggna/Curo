e01ce87cce2b664243e91ee9ab5206e35799352c
Erik Stenbäcka <stenbacka@gmail.com>
2012-07-01 18:38:59 +0200

    Adds basic client structure.

--- curo-client/urls.py
+++ curo-client/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+import os
+
+# TODO: This servers files from the client directory with the static.server
+#       functionality. This is not recommended for production use!
+
+PATH = os.path.dirname(os.path.realpath(__file__))
 
 urlpatterns = patterns('',
-
+    (r'^client/css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PATH + "/css/"}),
+    (r'^client/script/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PATH + "/script/"}),
+    (r'^client/partials/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PATH + "/partials/"}),
+    (r'^client/.*$', 'django.views.static.serve', {'path': "client.html", 'document_root': PATH + "/"})
 )
 
