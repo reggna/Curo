a947dba8183c6983caab50288f588ebf9ed28b1f
Gustav Tiger <gustav@tiger.name>
2012-07-10 22:46:38 +0200

    Add a basic client structure.
    - Some routes
    - Stubs for partials
    - Controller for main tabs
    - Add Twitter Bootstrap CSS file

--- curo-client/script/controllers.js
+++ curo-client/script/controllers.js
@@ -1,2 +1,13 @@
+function MainTabs($scope, $location, $log) {
+  $scope.tabs = [{title: 'Home',    url: '/client/home'},
+                 {title: 'Yearly',  url: '/client/yearly'},
+                 {title: 'Monthly', url: '/client/monthly'},
+                 {title: 'Add',     url: '/client/add'}];
 
+  $scope.getClass = function($index) {
+    var this_url = $scope.tabs[$index].url
+    var current_url = $location.path()
+    return (this_url == current_url) ? "active": "";
+  }
+}
 
