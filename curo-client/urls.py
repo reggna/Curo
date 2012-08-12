e0efc29effa92e85b116b793c1e42cf8682b1408
Henrik Steen <reggna@gmail.com>
2012-08-12 16:36:42 +0200

    Added a redirect from / to /client/home in curo-client/urls.py

--- curo-client/urls.py
+++ curo-client/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 import os
 
 # TODO: This servers files from the client directory with the static.server
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
     (r'^client/css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PATH + "/css/"}),
     (r'^client/script/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PATH + "/script/"}),
     (r'^client/partials/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PATH + "/partials/"}),
-    (r'^client/.*$', 'django.views.static.serve', {'path': "client.html", 'document_root': PATH + "/"})
+    (r'^client/.*$', 'django.views.static.serve', {'path': "client.html", 'document_root': PATH + "/"}),
+    (r'^$',  redirect_to, {'url': '/client/'}),
 )
 
