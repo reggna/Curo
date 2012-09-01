463e335081de27e3cc1babc9d1d136ae193f8f83
Erik Stenbäcka <stenbacka@gmail.com>
2012-09-01 19:35:27 +0200

    Adds on-blur-functionality.
    Ng does not provide a way to define events for on-blur.
    That feature is made available here in a way that corresponds to
    how ng defines its attribute defined events.

--- curo-client/script/widgets.js
+++ curo-client/script/widgets.js
@@ -1,2 +1,19 @@
 
-angular.module('CuroComponents', []);
+angular.module('CuroComponents', [])
+    .directive('ngBlur', function ($parse) {
+        return {
+            restrict: 'A',
+            link:
+                function postLink(scope, element, attrs) {
+                    if (attrs.ngBlur) {
+                        var fn = $parse(attrs.ngBlur);
+                        element.bind('blur', function (event) {
+                            scope.$apply(function () {
+                                fn(scope, {$event: event});
+                            });
+                        });
+                    }
+                }
+        };
+    });
+
